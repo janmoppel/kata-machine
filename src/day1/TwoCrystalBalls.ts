964d20f6f9e081903a4917fb576fba9b68090bfa
Jan Moppel <jan.moppel@fleetcomplete.com>
2024-10-07 12:10:14 +0300

    Implement two crystal balls solution

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,24 @@
+// Need to jump a good distance to find the "safe" place faster (sqrt(N)). Sqrt N is better than N
+// O(sqrt(N))
 export default function two_crystal_balls(breaks: boolean[]): number {
+  const jump = Math.floor(Math.sqrt(breaks.length));
 
+  let i = jump;
+  for (; i < breaks.length; i += jump) {
+    // 1. ball broke
+    if (breaks[i]) {
+      break;
+    }
+  }
+
+  // Last safe place
+  i -= jump;
+  // Walk linearly to the break point
+  for (let j = 0; j < jump && i < breaks.length; ++j, ++i) {
+    if (breaks[i]) {
+      return i;
+    }
+  }
+
+  return -1;
 }
