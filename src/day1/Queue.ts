8febf9efaf0d5bb8316e723995520cc53a908836
Jan Moppel <jan.moppel@fleetcomplete.com>
2024-10-08 13:07:41 +0300

    Implement Queue and Stack

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,58 @@
+type Node<T> = {
+    value: T,
+    next?: Node<T>
+}
+
+// FIFO - start from the head. Mono directional.
+// Head -> Tail
 export default class Queue<T> {
     public length: number;
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
-    
 
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
     }
 
+    // Add to Queue
     enqueue(item: T): void {
+        const node = { value: item } as Node<T>;
 
-}
+        this.length++;
+        if (!this.tail) {
+            this.tail = this.head = node;
+            return;
+        }
+
+        this.tail.next = node;
+        this.tail = node;
+    }
+
+    // Remove from Queue
     deque(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
 
-}
+        this.length--;
+
+        // Set the head to the next el
+        const head = this.head;
+        this.head = this.head.next;
+
+        // free head
+        head.next = undefined;
+        if (this.length === 0) {
+            this.tail = undefined;
+        }
+
+        return head.value;
+    }
+
     peek(): T | undefined {
-
-}
+        // Remember - first in first out!!
+        return this.head?.value;
+    }
 }
