45d6cba2f8f3fce1f0b4d52f930f8bd70d430536
Jan Moppel <jan.moppel@fleetcomplete.com>
2024-10-08 11:31:50 +0300

    Implement bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,13 @@
+// O(N^2)
 export default function bubble_sort(arr: number[]): void {
-
+  // At the end of each iteration -> the highest number is at the end
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j+1]) {
+        const value = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = value;
+      }
+    }
+  }
 }
