8d64ed549a4a25ef0dd26103283549c016a56caa
Jan Moppel <jan.moppel@fleetcomplete.com>
2024-10-11 12:34:38 +0300

    Implement Binary tree operations

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,24 @@
+// Breadth-First search
+// Return the node, while being at this level
+// Depth-First -> Stack. Breadth-first -> Queue;
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+  const q = [head];
 
+  while (q.length) {
+    const curr = q.shift() as BinaryNode<number>;
+
+    // search
+    if (curr.value === needle) {
+      return true;
+    }
+
+    if (curr.left) {
+      q.push(curr.left);
+    }
+    if (curr.right) {
+      q.push(curr.right);
+    }
+  }
+
+  return false;
 }
--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,19 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+function search(curr: BinaryNode<number> | null, needle: number): boolean {
+  if (curr === null) {
+    return false;
+  }
+  if (curr.value === needle) {
+    return true;
+  }
 
+  // This is BINARY TREE !! ( <= P < )
+  if (curr.value < needle) {
+    return search(curr.right, needle);
+  }
+
+  return search(curr.left, needle);
+}
+
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+  return search(head, needle);
 }
